d4da894a611289e1e352ada3b248dd5cd4fe8a23
Loriick <loricktravailleur@gmail.com>
2023-08-30 18:53:21 +0200

    refactor: Procfile

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.use(cookieParser());
-  await app.listen(process.env.PORT || 3001);
+  app.enableCors();
+  await app.listen(parseInt(process.env.PORT) || 3001);
 }
 bootstrap();
